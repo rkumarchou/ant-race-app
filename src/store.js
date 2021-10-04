e7bbac94e9247498f35096a77a4d508915bdffa1
rkumarchou <riteshchoudhary2605@gmail.com>
2021-10-04 17:21:34 +0530

    Added store.js file

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
+export default function configureStore() {
+  return createStore(
+    reducer,
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+}
